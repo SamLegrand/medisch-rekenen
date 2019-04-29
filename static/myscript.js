5813347b2d0a2423c64989cfcc1cef0b0b717d73
SamLegrand <37298570+SamLegrand@users.noreply.github.com>
2019-04-29 18:03:34 +0200

    Implemented evaluation application

--- static/myscript.js
+++ static/myscript.js
@@ -1,4 +1,103 @@
 $(document).ready(function(){
+    let input = {};
+    let current = 0;
+    let testEnd = 0;
+    let score = 0;
+    let time = 0;
+    const gaugeWidth = 150;
+    let progressUnit = 0;
+    let counter = 0;
+    let TIMER;
+    
+    function renderQuestion() {
+        let q = input.questions[current];
+        $('#question').html(q.question);
+        $('#A').parent().find('p').html(q.A);
+        $('#B').parent().find('p').html(q.B);
+        $('#C').parent().find('p').html(q.C);
+        $('#D').parent().find('p').html(q.D);
+        if (current == input.questions.length - 1) {
+            $('#proceed').html('Evaluatie voltooien');
+        }
+    }
+    
+    function updateScore() {
+        $('#score').find('p').html(score + '/' + input.questions.length);
+    }
+    
+    function renderScore() {
+        $('#scoreCard').find('h5').html('U behaalde ' + score + '/' + input.questions.length);
+        $('#evaluation').slideUp();
+        $('#scoreCard').slideDown();
+    }
+    
+    function renderTimer() {
+        if (counter <= time) {
+            $('#timer').html(counter);
+            //$('#timeGauge').style.width = progressUnit * counter + "px";
+            counter++;
+        }
+        else {
+            clearInterval(TIMER);
+            renderScore();
+        }
+    }
+    
+    function startTest() {
+        renderTimer();
+        TIMER = setInterval(renderTimer, 1000);
+        updateScore();
+        renderQuestion();
+        $('#instructions').slideUp();
+        $('#evaluation').slideDown();
+    }
+    
+    $('#to-test').on('click', function(e) {
+        let selectedOption = $('select[name="year"] option:selected');
+        console.log('selected option: ' + selectedOption.val());
+        $.getJSON('/static/tests/' + selectedOption.val() + '.json', function(data) {
+            input = data;
+            testEnd = input.questions.length - 1;
+            time = input.time;
+            progressUnit = gaugeWidth/time;
+            startTest();
+        });
+    });
+    
+    $('.choice').on('click', function(e) {
+        let choice = $(this);
+        // disable choices
+        $('#evaluation input').each(function() {
+            $(this).prop('disabled', true);
+        });
+        // answer is correct
+        if (input.questions[current].correct == $(this).attr('id')) {
+            choice.parent().find('p').css('color', 'green');
+            score++;
+            updateScore();
+        }
+        // answer is not correct
+        else {
+            choice.parent().find('p').css('color', 'red');
+            $('#' + input.questions[current].correct).parent().find('p').css('color', 'green');
+        }
+        $('#proceed').show();
+    });
+    
+    $('#proceed').on('click', function(e) {
+        // render next question if it exists
+        if (current < testEnd) {
+            current++;
+            renderQuestion();
+        }
+        // end test
+        else {
+            clearInterval(TIMER);
+            renderScore();
+        }
+        $(this).hide();
+    });
+
     $('#LoginForm').submit(function(e) {
         e.preventDefault();
         $.ajax({
@@ -72,5 +171,7 @@ $(document).ready(function(){
          $('#welcome').slideDown();
          $('#instructions').slideUp();
     });
+    
+    
     
 });
